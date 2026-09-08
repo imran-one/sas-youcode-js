c2e73c75ec7e32a25262a6d4c91a0fdaae90208a
imran benmiloud <benmiloudimran@gmail.com>
2026-09-08 23:32:32 +0200

    calcule lengeur de string

--- day01/exercices/exercice-16.js
+++ day01/exercices/exercice-16.js
@@ -15,3 +15,10 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
+
+let numero = "067919852";
+if(numero.length===10){
+    console.log("Numéro valide");
+}else{
+    console.log("Numéro invalide");
+}
