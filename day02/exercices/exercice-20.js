63bc3123b1e7e600f3cd3114dc276f2840f363c5
imran benmiloud <benmiloudimran@gmail.com>
2026-09-09 23:50:46 +0200

    .

--- day02/exercices/exercice-20.js
+++ day02/exercices/exercice-20.js
@@ -14,4 +14,14 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+for (let n = 100; n <= 999; n++) {
+    let a = parseInt(n / 100);
+    let b = parseInt(n / 10) % 10;
+    let c = n % 10;
+
+    if (a ** 3 + b ** 3 + c ** 3 == n) {
+        console.log(n);
+    }
+}
