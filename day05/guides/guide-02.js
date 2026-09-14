99ba7399e8de09f9e72a890136211b2f6a9bba67
imran-one <benmiloudimran@gmail.com>
2026-09-14 15:47:28 +0200

    .

---
+++ day05/guides/guide-02.js
@@ -0,0 +1,14 @@
+/**
+ * Jour 05 — GUIDE 02
+ * Recherche de mot-clé (Sensibilité à la casse)
+ *
+ * OBJECTIF
+ * Vérifiez si le mot "javascript" est présent dans la phrase "J'apprends le JavaScript à YouCode". La recherche doit ignorer les majuscules/minuscules.
+ *
+ * Aide : consulte ../02-guides.md seulement si tu bloques.
+ */
+'use strict';
+
+let mot = "javascript"
+let phrase = "J'apprends le JavaScript à YouCode"
+
