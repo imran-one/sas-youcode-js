8294238824d598ad6b4175bb4eedb8139dcd7c00
imran-one <benmiloudimran@gmail.com>
2026-09-15 12:27:52 +0200

    REGEX BASIQUE

--- day05/exercices/exercice-19.js
+++ day05/exercices/exercice-19.js
@@ -5,7 +5,10 @@
  * ─────────────────────────────────────────────────────────────
  *
  * 🎯 MISSION
- * Simulez la vérification d'une plaque d'immatriculation marocaine. Le format attendu est "1234-A-56" ou "12345-AB-6". Pour simplifier, vérifiez qu'elle contient deux tirets et qu'une des sections au milieu est une lettre. L'utilisation d'expressions régulières (Regex) est recommandée ici !
+ * Simulez la vérification d'une plaque d'immatriculation marocaine.
+ *  Le format attendu est "1234-A-56" ou "12345-AB-6".
+ *  Pour simplifier, vérifiez qu'elle contient deux tirets et qu'une des sections au milieu est une lettre.
+ *  L'utilisation d'expressions régulières (Regex) est recommandée ici !
  *
  * 📖 Consigne détaillée : ../03-exercices.md#exercice-19
  * ▶️ Commande : node day05/exercices/exercice-19.js
@@ -14,4 +17,14 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function vérification(plaque){
+    let v =/[a-zA-Z]/ 
+    let m = plaque.split('-')
+    if (m.length !== 3) return false
+    return v.test(m[1])
+}
+
+console.log(vérification("12345-AB-6"));
+ console.log(vérification("1-234-A-56"));
+
