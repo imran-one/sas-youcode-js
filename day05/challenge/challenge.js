d52395a4b4178c52e8f0646f7f61057994062ca4
imran-one <benmiloudimran@gmail.com>
2026-09-15 15:21:05 +0200

    lesTableaux

--- day05/challenge/challenge.js
+++ day05/challenge/challenge.js
@@ -10,7 +10,8 @@
  * Consignes :
  * 1. Vous avez une liste (tableau) de mots interdits : ["noob", "idiot", "nul"].
  * 2. Créez une fonction filtrerMessage(message) qui reçoit le message tapé par le joueur.
- * 3. Si le message contient un des mots interdits (peu importe s'il y a des majuscules, ex: "nOOb"), il doit être remplacé par des astérisques de la même longueur (ex: "").
+ * 3. Si le message contient un des mots interdits (peu importe s'il y a des majuscules, ex: "nOOb"), 
+ * il doit être remplacé par des astérisques de la même longueur (ex: "").
  * 4. Retournez le message censuré.
  *
  * Exemple de test :
@@ -23,4 +24,23 @@
 'use strict';
 
 // Découpe d'abord le problème en petites étapes.
-// TODO: écris ta solution ici.
+
+let interdits = ["noob", "idiot", "nul"]
+let star = '*'
+
+function filtrerMessage(message) {
+    let texte = message 
+    message = message.split(" ")
+    texte = texte.toLowerCase()
+    texte= texte.split(" ")
+    for (let i = 0; i < interdits.length; i++) {
+        for (let j = 0; j < texte.length; j++) {
+            if (interdits[i] == texte[j]) {
+                message[j] = star.repeat(message[j].length)
+            }
+        }
+    }
+
+    return message.join(" ")
+}
+console.log(filtrerMessage("Tu es vraiment un GROS NooB et un idiot !!"));
